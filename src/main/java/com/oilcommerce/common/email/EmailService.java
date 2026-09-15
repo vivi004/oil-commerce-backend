bc1fa31b60145f213b3111868b8c06d6b4e18452
Vivin.M <vivinmani20@gmail.com>
2026-09-15 21:22:20 +0530

    perf(mail): make email sending asynchronous with SMTP timeouts for instant UI response

--- src/main/java/com/oilcommerce/common/email/EmailService.java
+++ src/main/java/com/oilcommerce/common/email/EmailService.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -21,6 +22,7 @@ public class EmailService {
     @Value("${spring.mail.username:}")
     private String fromEmail;
 
+    @Async
     public void sendPasswordResetEmail(String toEmail, String token) {
         String resetLink = frontendUrl + "/auth/reset-password?token=" + token;
 
